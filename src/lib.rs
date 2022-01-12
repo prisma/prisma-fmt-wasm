8c56b1b26eab23944ef790ee109d5356046b0e29
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2022-01-12 11:18:17 +0200

    Apply suggestions from code review
    Co-authored-by: Tom Houlé <13155277+tomhoule@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -46,6 +46,7 @@ pub fn debug_panic() {
     panic!("This is the panic triggered by `prisma_fmt::debug_panic()`");
 }
 
+#[cfg(feature = "wasm_logger")]
 #[wasm_bindgen]
 pub fn enable_logs() {
     wasm_logger::init(wasm_logger::Config::default());
