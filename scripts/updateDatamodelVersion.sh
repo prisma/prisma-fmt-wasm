e74484050da00fb1181ad331863239fff709fea4
Tom Houlé <13155277+tomhoule@users.noreply.github.com>
2022-06-25 09:19:42 +0200

    Rename updateLocks to updateDatamodelVersion and make it more robust (#30)
    - Rename updateLocks to updateDatamodelVersion
    - Stop updating other dependencies when updating datamodel dependency,
      instead do it during the weekly sunday upgrades.
    - Fail hard if enginesHash is not provided.
    
    The general aim of the PR is to isolate the failure mode in
    https://github.com/prisma/prisma-fmt-wasm/issues/29 as much as possible.

--- scripts/updateDatamodelVersion.sh
+++ scripts/updateDatamodelVersion.sh
@@ -1,14 +1,15 @@
-# Updates:
+# Updates the checksums for the datamodel crate in:
 # - Cargo.lock
 # - datamodel-0.1.0.sha256sum
 set -euxo pipefail
 export DATAMODEL_CHECKSUM_FILE=datamodel-0.1.0.sha256sum
 
-echo 'Running cargo update...'
-cargo update
-
 if [[ ${enginesHash:?} != "" ]]; then
+  echo "Updating to enginesHash=${enginesHash:?}"
   cargo update -p datamodel --precise "$enginesHash"
+else
+  echo "/! \ ERROR /!\ No enginesHash was passed in for this script to update to."
+  exit 1
 fi
 
 echo 'Setting up fake checksum so the build can fail and output the new hash...'
